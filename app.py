1c0aad891c41fb6c1b08530a72561ca3e9998fa6
Anna Polyanskaya <31651460+polyankaglade@users.noreply.github.com>
2022-08-18 22:17:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,7 @@ def index():
            </form>'''
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
